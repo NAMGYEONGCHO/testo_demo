77c4a9a3de356a13924fc086a7bebb4421909cee
NAMGYEONGCHO <official2002mk@gmail.com>
2023-05-13 19:41:36 +0200

    setup tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,29 @@ async function fetchData() {
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <DataDisplay />
+      <div className="flex flex-col h-screen">
+        <header className="bg-gray-800 p-4">
+          <nav className="container mx-auto flex justify-between items-center">
+            <h1 className="text-white font-bold text-xl">Carbon Emission Monitoring</h1>
+            <button className="text-white font-bold">Logout</button>
+          </nav>
+        </header>
+        <main className="container mx-auto flex-grow px-4">
+          <h2 className="text-3xl font-bold mt-6">Welcome to the Dashboard</h2>
+          <p className="text-lg mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vehicula ex vel sem luctus, eu aliquam velit tincidunt. Donec eget diam commodo, ultricies augue nec, dictum mauris. Etiam volutpat tellus vel urna commodo, sed scelerisque urna interdum. Morbi porttitor imperdiet felis at tristique.</p>
+          <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-6">Get Started</button>
+        </main>
+        <footer className="bg-gray-800 py-2">
+          <div className="container mx-auto">
+            <p className="text-center text-white">© 2023 Carbon Emission Monitoring. All rights reserved.</p>
+          </div>
+        </footer>
+      </div>
+
     </QueryClientProvider>
   );
 }
 
 export default App;
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,36 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'weather-blue': '#1E213A',
+          'weather-light-blue': '#3C47E9',
+          'weather-card': '#1E213A',
+          'weather-gray': '#6E707A',
+          'weather-light-gray': '#A09FB1',
+          'weather-lightest-gray': '#E7E7EB',
+          'weather-white': '#100E1D',
+        },
+        backgroundImage: theme => ({
+          'weather-cloudy': "url('/src/assets/cloudy.svg')",
+          'weather-sunny': "url('/src/assets/sunny.svg')",
+        }),
+        spacing: {
+          '72': '18rem',
+          '84': '21rem',
+          '96': '24rem',
+        },
+        screens: {
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1536px',
+        }
+      },
     },
     variants: {
       extend: {},
     },
     plugins: [],
   };
-  
